37f51e1f53042222b649eaf1b306f33d4fbc07cc
CoderBDK <110069678+CoderBDK@users.noreply.github.com>
2022-12-09 00:05:26 +0600

    Computer

---
+++ ComputerActivity.java
@@ -0,0 +1,28 @@
+package com.tic.tac.toe;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ComputerActivity extends Activity
+{
+
+	private Game game;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		game=new Game(this);
+		
+	
+	}
+
+	@Override
+	public void onBackPressed()
+	{
+		super.onBackPressed();
+		game.reset();
+		finish();
+	}
+	
+}
